2a15e5574f8ac79323315b221e24fb7b67ed1120
ian05012 <ian31333025@gmail.com>
2021-12-12 19:36:24 +0800

    inner-aside

---
+++ js/inner-aside.js
@@ -0,0 +1,17 @@
+var el = document.querySelector('aside');
+
+el.innerHTML=
+
+	'<h3 class="sub-title">類別</h3>' +
+	
+	'<button class="accordion">從0開始用Java編寫Discord 機器人!</button>' +
+	
+	'<div class="panel"><ul class="sub-menu">' +
+	
+	'<li><a href="java_discordbot_01.html">[1]讓機器人運行吧!</a></li>' +
+	
+	'<li><a href="java_discordbot_02.html">[2]監聽事件</a></li></ul></div>' +
+	
+	'<h3 class="sub-title">關於這裡</h3>' +
+	
+	'<p>這裡可能會放一些小東的各種廢話有可能會有程式上的心得分享，或者是生活中一些趣事，抑或是遊戲上的酷東西。大家可以來看看歐</p>';
